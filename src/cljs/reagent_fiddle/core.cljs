6ab39b6c2fe1e91e68ac922e67d417db9eab53e3
sssmi <simeon.kerkola@gmail.com>
2018-07-24 11:04:42 +0300

    Add new Cards and autofocus component

--- src/cljs/reagent_fiddle/core.cljs
+++ src/cljs/reagent_fiddle/core.cljs
@@ -8,53 +8,67 @@
 (def state (r/atom {:columns [{:id (random-uuid)
                                :title "Todos"
                                :cards [{:id (random-uuid)
-                                        :text "Learn about Reagent"}
+                                        :title "Learn about Reagent"}
                                        {:id (random-uuid)
-                                        :text "Go to sleep"}]}
+                                        :title "Go to sleep"}]}
                               {:id (random-uuid)
                                :title "Buy"
                                :cards [{:id (random-uuid)
-                                        :text "Groceries"}
+                                        :title "Groceries"}
                                        {:id (random-uuid)
-                                        :text "What ever"}]}]}))
+                                        :title "What ever"}]}]}))
 
-(defn- update-text [card-cur text]
-  (swap! card-cur assoc :text text)
-  (println text))
+(defn auto-focus-input [props]
+  (r/create-class
+   {:display-name "auto-focus-input"
+    :component-did-mount (fn [component]
+                           (.focus (r/dom-node component)))
+    :reagent-render (fn [props]
+                      [:input props])}))
 
-(defn- stop-editing [card-cur]
-  (swap! card-cur dissoc :editing)
+(defn- update-title [cursor title]
+  (swap! cursor assoc :title title)
+  (println title))
+
+(defn- stop-editing [cursor]
+  (swap! cursor dissoc :editing)
   (println "New state is:" @state))
 
-(defn- start-editing [card-cur]
-  (swap! card-cur assoc :editing true))
+(defn- start-editing [cursor]
+  (swap! cursor assoc :editing true))
 
-(defn card [card-cur]
-  (let [{:keys [editing text]} @card-cur]
+(defn editable [el cursor]
+  (let [{:keys [editing title]} @cursor]
     (if editing
       ; If editing, show input field
-      [:div.card.editing [:input {:type "text"
-                                  :value text
-                                  :autoFocus true
-                                  :on-change #(update-text card-cur (.. % -target -value))
-                                  ; When the text field is not selected anymore
-                                  :on-blur #(stop-editing card-cur)
-                                  ; When hit enter, stop editing
-                                  :on-key-press #(if (= (.-charCode %) 13)
-                                                   (stop-editing card-cur))}]]
-      [:div.card {:on-click #(start-editing card-cur)} text])))
+      [el {:className "editing"}
+       [auto-focus-input {:type "title"
+                           :value title
+                           :on-change #(update-title cursor (.. % -target -value))
+                           ; When the title field is not selected anymore
+                           :on-blur #(stop-editing cursor)
+                           ; When hit enter, stop editing
+                           :on-key-press #(if (= (.-charCode %) 13)
+                                             (stop-editing cursor))}]]
+      [el {:on-click #(start-editing cursor)} title])))
 
-(defn new-card []
+(defn card [cursor]
+  [editable :div.card cursor])
+
+(defn- add-new-card [col-cur]
+  (swap! col-cur update :cards conj {:id (random-uuid)
+                                     :title ""
+                                     :editing true}))
+
+(defn new-card [col-cur]
   [:div.new-card
+   {:on-click #(add-new-card col-cur)}
    "+ a new card"])
 
 (defn column [col-cur]
-  (let [{:keys [title cards editing]} @col-cur]
+  (let [{:keys [title cards]} @col-cur]
     [:div.column
-     (if editing
-       ; If editing, show input field
-       [:input {:type "text" :value title}]
-       [:h2 title])
+     ^{:key title}[editable :h2 col-cur]
 
       ; Get each individual card
      (map-indexed (fn [idx {id :id}]
@@ -63,10 +77,17 @@
                     (let [card-cur (r/cursor col-cur [:cards idx])]
                       ^{:key id} [card card-cur]))
                   cards)
-     [new-card]]))
+     [new-card col-cur]]))
 
-(defn new-column []
+(defn- add-new-column [board]
+  (swap! state update :columns conj {:id (random-uuid)
+                                     :title ""
+                                     :cards []
+                                     :editing true}))
+
+(defn new-column [board]
   [:div.new-column
+   {:on-click #(add-new-column board)}
    "+ new column"])
 
 (defn board [board]
@@ -75,7 +96,7 @@
                   (let [col-cur (r/cursor board [:columns idx])]
                     ^{:key id} [column col-cur]))
                 (:columns @board))
-   [new-column]])
+   [new-column board]])
    ; ; Loop thru columns from the state, and make a component for each
    ; (for [c (:columns @state)]
    ;   ^{:key c} [column c])
